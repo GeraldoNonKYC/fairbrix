1374b7d43a16414b2e74664db06179a8a50868b3
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-01-13 03:06:17 +0000

    refactor: Rename Litecoin/litecoin/LTC/ltc to Fairbrix/fairbrix/FBX/fbx

--- src/wallet/coinselection.h
+++ src/wallet/coinselection.h
@@ -105,12 +105,12 @@ struct CoinEligibilityFilter
 };
 
 enum class InputPreference {
-    // Use LTC and MWEB inputs (MIXED)
+    // Use FBX and MWEB inputs (MIXED)
     ANY,
     // Only use MWEB inputs (used when explicitly pegging-out)
     MWEB_ONLY,
     // Only use canonical inputs (used when explicitly pegging-in)
-    LTC_ONLY
+    FBX_ONLY
 };
 
 struct OutputGroup
