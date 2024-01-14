91b7c173a246a055f1a9c3c671e2cf9cb02791c1
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-01-14 22:05:06 +0000

    fix: Add Multicoin.co DNS Seed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -125,7 +125,7 @@ public:
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
         vSeeds.emplace_back("fbx.kawaii.kiwi");
-        vSeeds.emplace_back("multicoin.co");
+        vSeeds.emplace_back("fairbrix.dnsseed.multicoin.co");
 
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,95);
